50a13029ba0b1c92c2e5e00240fd9705816041be
Erica Winberry <elaewin@gmail.com>
2016-11-15 10:23:27 -0800

    first part of demo.

--- Objective-C_Demo/Objective-C_Demo/ViewController.m
+++ Objective-C_Demo/Objective-C_Demo/ViewController.m
@@ -46,6 +46,70 @@ int gMoveNumber = 10;
     // calling a class method - note UC initial letter! - same syntax for Type method
     [Person sayHello];
 
+    // MARK: Day 2 Demos
+    
+    NSNumber *number1 = @123;
+    NSNumber *number2 = @321;
+    
+    // NSNumber has its own method of comparison, so use THAT method - and use the one that's specific to THAT data type.
+    if([number1 isEqualToNumber: number2]) {
+        NSLog(@"First number is bigger!");
+    }
+
+    // using compare method that comes with NSNumber.
+    switch ([number1 compare: number2]) {
+        case NSOrderedSame:
+            NSLog(@"Values are equal");
+            break;
+        case NSOrderedAscending:
+            NSLog(@"Values are ascending.");
+            break;
+        case NSOrderedDescending:
+            NSLog(@"Values are descending.");
+            break;
+        default:
+            break;
+    }
+    
+    // Showing that string formatter knows how to display the string version of an object -- gives the type and the memory address for that particular instance.
+    ViewController *myVC = [[ViewController alloc]init];
+    
+    NSString *vcString = [NSString stringWithFormat: @"VC string: %@", myVC];
+    NSLog(@"%@", vcString);
+    
+    // More string demo:
+    
+    NSString *string = [NSString stringWithFormat: @"%@, %@", number1, number2];
+    
+    // This is creating a new NSString and assigning it to the reference 'string' - these are still immutable strings.
+    string = @"This is a new string...";
+    
+    // Mutable strings
+    
+    NSMutableString *mutableString = [NSMutableString stringWithFormat: @"%@, %@", number1, number2];
+    
+    [mutableString appendString: @" plus another addition, because mutable!"];
+    
+    NSLog(@"%@", mutableString);
+    
+    
+    // Type changing problems!
+    
+    NSString *stringy = @"This is a string with class";
+    
+    NSLog(@"%@, %@", stringy, stringy.class);
+    
+    stringy = [NSMutableString stringWithFormat:@"%@", stringy];
+    
+    NSLog(@"%@ that is now %@", stringy, stringy.class);
+    
+    // but can't access mutable methods on stringy!
+    // (test this here with append method!)
+    
+    // In order to cast the type as mutable from immutable and get autocomplete access to the methods for the immutable type (convince the compiler that the methods are available), have to actually do something to change the string that is now mutable! This will also only work one statment at a time. Eg. it is only cast as mutable for the purpose of one statment, according to what the compiler knows.
+    [(NSMutableString *)stringy appendFormat:@" (but after recasting the type, the mutable string methods will autocomplete.)"];
+    
+    
 }
 
 

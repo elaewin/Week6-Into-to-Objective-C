2c6337e62d6e7482d28767518111e08ecd986881
Erica Winberry <elaewin@gmail.com>
2016-11-16 12:12:19 -0800

    Student type creation finished.

--- ClassRoster/ClassRoster/Student.h
+++ ClassRoster/ClassRoster/Student.h
@@ -8,9 +8,14 @@
 
 #import <Foundation/Foundation.h>
 
-@interface Student : NSObject
+@interface Student : NSObject<NSCopying, NSCoding>
 
 @property(strong, nonatomic)NSString *firstName;
+@property(strong, nonatomic)NSString *lastName;
+@property(strong, nonatomic)NSString *email;
+
+
+-(instancetype)initWithFirstName:(NSString *)firstName lastName:(NSString *)lastName email:(NSString *)email;
 
 
 
